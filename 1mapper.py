4a147edc340b82cf5a5bfa5a9784bbca7db36153
pathuribhavani <bavani.pathuri@gmail.com>
2020-02-12 13:19:37 -0600

    changes in mapper and reducer

--- 1mapper.py
+++ 1mapper.py
@@ -4,7 +4,7 @@ output = open("01.txt", "w")
 for line in input:
     datalist = line.strip().split("    ")
     date, time,store, item, cost, paymentType = datalist
-    output.write(store + "\t" + cost + "\n")
+    output.write(paymentType + "\t" + cost + "\n")
 
 input.close()
 output.close()
